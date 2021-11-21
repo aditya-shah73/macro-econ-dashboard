0cc1846d7091ed7743e813c5f4e07cb1022eacca
Sahithi <sahithikhandavalli@gmail.com>
2021-11-20 18:31:30 -0800

    added range slider to pick years

--- src/containers/Timeline.js
+++ src/containers/Timeline.js
@@ -1,5 +1,38 @@
 import React from "react";
 import { connect } from "react-redux";
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+const marks = [
+{
+value: 1960,
+label: '1960',
+},
+{
+value: 1970,
+label: '1970',
+},
+{
+value: 1980,
+label: '1980',
+},
+{
+value: 1990,
+label: '1990',
+},
+{
+value: 2000,
+label: '2000',
+},
+{
+value: 2010,
+label: '2010',
+},
+{
+value: 2020,
+label: '2020',
+}
+];
 
 function Timeline({
   selectedYearFrom,
@@ -7,8 +40,27 @@ function Timeline({
   updateYearFrom,
   updateYearTo,
 }) {
+
+const [value, setValue] = React.useState([selectedYearFrom, selectedYearTo]);
+
+const handleChange = (event, newValue) => {
+    updateYearFrom(newValue[0]);
+    updateYearTo(newValue[1]);
+    setValue(newValue);
+  };
   return (
     <div id="timeline">
+    <Box sx={{ width: 1000,m: 2 }}>
+          <Slider
+            getAriaLabel={() => 'Year range'}
+            value={value}
+            min = {1960}
+            max = {2020}
+            marks = {marks}
+            onChange={handleChange}
+            valueLabelDisplay="auto"
+          />
+        </Box>
       Timeline {selectedYearFrom}-{selectedYearTo}
     </div>
   );
